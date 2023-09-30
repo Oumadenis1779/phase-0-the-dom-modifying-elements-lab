2afd4fc2bceb50ec5602072d545a0cc3fd321f03
Oumadenis1779 <dennies.ouma@student.moringaschool.com>
2023-09-30 17:36:46 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const mainElement = document.querySelector('main#main');
+mainElement.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+newHeader.innerHTML = 'YOU-NAME is the champion';
+document.body.appendChild(newHeader)
